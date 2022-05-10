15df5a2f0ba29844782397260f97e1139648a005
Francesco Bolognesi <francesco.bolognesi7@studio.unibo.it>
2022-05-10 19:58:48 +0200

    primi passi

--- epidemic.hpp
+++ epidemic.hpp
@@ -1,5 +1,23 @@
 #include <vector>
 
-//vettore di giorni. In ogni giorno, c'è una struct con R,S,I.
-//verifico con assert che siano sempre maggiori di 0.
-//altra idea: fare un vettore 
+// vettore di giorni. In ogni giorno, c'è una struct (population) con S,I,R.
+// verifico con assert che siano sempre maggiori di 0.
+//  E se invece di un vettore di giorni, facessi come nel lab5 un evolve con
+//  numero di giorni?
+
+struct population {
+  int S;
+  int I;
+  int R;
+};
+
+class epidemia {
+  std::vector<population> days(double delta_t);
+
+  for (int i = 0; i != delta_t; ++i) {
+    if (i = 0) {
+      days[0] = {int S0, int I0, int R0};
+    };  else {
+    };
+  };
+};
