c75a80c5be1b66269b7463525e229427b12c98a2
Andrea Fraternali <andrea.fraternali4@studio.unibo.it>
2022-05-11 17:42:24 +0200

    Fixed Bolo disasters

--- epidemic.hpp
+++ epidemic.hpp
@@ -1,23 +1,34 @@
-#include <vector>
+#ifndef EPIDEMIC_HPP
+#define EPIDEMIC_HPP
 
+#include <vector>
+#include <ostream>
 // vettore di giorni. In ogni giorno, c'è una struct (population) con S,I,R.
 // verifico con assert che siano sempre maggiori di 0.
 //  E se invece di un vettore di giorni, facessi come nel lab5 un evolve con
 //  numero di giorni?
 
-struct population {
+struct day {
   int S;
   int I;
   int R;
 };
 
-class epidemia {
-  std::vector<population> days(double delta_t);
+std::ostream& operator<<(day const& d, std::ostream& os){
+  return os << "S : " << d.S << ", I : " << d.I << ", R : " << d.R;
+}
 
-  for (int i = 0; i != delta_t; ++i) {
-    if (i = 0) {
-      days[0] = {int S0, int I0, int R0};
-    };  else {
-    };
-  };
-};
+// class epidemia {
+//   std::vector<day> days(double delta_t);
+
+//   for (int i = 0; i != delta_t; ++i) {
+//     if (i = 0) {
+//       days[0] = {int S0, int I0, int R0};
+//     } 
+//     else {
+//     }
+//   }
+// };
+
+
+#endif
