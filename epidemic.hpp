16430fedff04c13f6b3c896291ec586ef6bfe230
Francesco Bolognesi <francesco.bolognesi7@studio.unibo.it>
2022-05-11 16:15:55 +0200

    updating epidemic.hpp

--- epidemic.hpp
+++ epidemic.hpp
@@ -3,7 +3,7 @@
 // vettore di giorni. In ogni giorno, c'è una struct (population) con S,I,R.
 // verifico con assert che siano sempre maggiori di 0.
 //  E se invece di un vettore di giorni, facessi come nel lab5 un evolve con
-//  numero di giorni?
+//  numero di giorni ?
 
 struct population {
   int S;
