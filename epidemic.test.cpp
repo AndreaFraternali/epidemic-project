b2497a2132b189dbe81d668573d2fde914094bec
Andrea Doddi <andrea.doddi@studio.unibo.it>
2022-05-19 15:44:34 +0200

    adding first test

--- epidemic.test.cpp
+++ epidemic.test.cpp
@@ -5,13 +5,15 @@
 
 TEST_CASE("testing evolve") {
   SUBCASE("tot persone, tot giorni") {
-    double gamma = ;
-    double beta = ;
-    double v_ratio = ;
-    Day today{};
+    double gamma = 0.046 ;
+    double beta = 0.056 ;
+    double v_ratio = 0;
+    Day today{9980, 20, 0};
     Epidemic epidemic{gamma, beta, v_ratio, today};
-    epidemic.evolve();
+    epidemic.evolve(1);
     auto s = epidemic.state();
-    CHECK(s == Day{});
+    CHECK(s == Day{9979, 20, 1});
   }
+
+
 }
