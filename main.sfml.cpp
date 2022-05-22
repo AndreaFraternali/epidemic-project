a096dd2df166d4d698a605c8c0afe62343751164
Andrea Fraternali <andrea.fraternali4@studio.unibo.it>
2022-05-22 23:21:50 +0200

    add legend

--- main.sfml.cpp
+++ main.sfml.cpp
@@ -41,7 +41,7 @@ int main() {
   double delta_x = 0.01 * display_width;
   double delta_y = 0.02 * display_height;   
 
-
+  //setting degli assi cartesiani
   y_axis[0].position = origin;
   y_axis[1].position = sf::Vector2f(origin.x, ymax - delta_y);
   y_axis[0].color = sf::Color::Black;
@@ -51,15 +51,43 @@ int main() {
   x_axis[1].position = sf::Vector2f(xmax + delta_x, origin.y);
   x_axis[0].color = sf::Color::Black;
   x_axis[1].color = sf::Color::Black;
+   
 
   sf::CircleShape Spoint{3};
   Spoint.setFillColor(sf::Color::Green);
-
   sf::CircleShape Ipoint{3};
   Ipoint.setFillColor(sf::Color::Red);
-
   sf::CircleShape Rpoint{3};
   Rpoint.setFillColor(sf::Color::Blue);
+
+
+  //Setting degli oggetti per la legenda
+  sf::Font font{};
+  if(!font.loadFromFile("times.ttf")){
+    throw std::runtime_error("Opening font file failed");
+  }
+
+  sf::Text legS{"Suscettibili", font, 24};
+  legS.setFillColor(sf::Color::Black);
+  legS.setPosition(0.7 * display_width, 0.05 * display_height);
+  sf::Text legI{"Infetti", font, 24};
+  legI.setFillColor(sf::Color::Black);
+  legI.setPosition(0.7 * display_width, 0.09 * display_height);
+  sf::Text legR{"Rimossi", font, 24};
+  legR.setFillColor(sf::Color::Black);
+  legR.setPosition(0.7 * display_width, 0.13 * display_height); 
+
+  sf::CircleShape Scirc{8};
+  Scirc.setFillColor(sf::Color::Green);
+  Scirc.setPosition(0.85 * display_width, 0.065 * display_height);
+  sf::CircleShape Icirc{8};
+  Icirc.setFillColor(sf::Color::Red);
+  Icirc.setPosition(0.85 * display_width, 0.105 * display_height);
+  sf::CircleShape Rcirc{8};
+  Rcirc.setFillColor(sf::Color::Blue);
+  Rcirc.setPosition(0.85 * display_width, 0.145 * display_height);
+
+
 
   // game loop
   while (window.isOpen()) {
@@ -75,6 +103,14 @@ int main() {
     window.draw(y_axis);
     window.draw(x_axis);
 
+    window.draw(legS);
+    window.draw(legI);
+    window.draw(legR);
+
+    window.draw(Scirc);
+    window.draw(Icirc);
+    window.draw(Rcirc);
+
     for (int i = 0; i != days ; i++) {
       Spoint.setPosition(ConvertCoordinates(
           sf::Vector2f(i * xscale, evolution[i].S * yscale),
@@ -89,6 +125,15 @@ int main() {
       window.draw(Ipoint);
       window.draw(Rpoint);
     }
+
+    //da sistemare
+    for(double i = origin.x; i < xmax ; i += 10 * xscale){
+      sf::Text num{std::to_string((i - origin.x)/xscale), font, 18};
+      num.setFillColor(sf::Color::Black);
+      num.setPosition(i, origin.y + delta_y);
+      window.draw(num);
+    }
+      
 
     window.display();
   }
