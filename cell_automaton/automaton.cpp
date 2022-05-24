34bb55ea17d9eebb3e2be9aabafab646d94b4082
Francesco Bolognesi <francesco.bolognesi7@studio.unibo.it>
2022-05-24 12:19:46 +0200

    .

--- cell_automaton/automaton.cpp
+++ cell_automaton/automaton.cpp
@@ -1,4 +1,12 @@
-#include "automaton.hpp"
 
-//uso random per mettere un suscettibile in un punto a caso
+//uso random per mettere un suscettibile in un punto a caso (se si può fare)
 //uso random per confrontarlo con probabilità gamma di infettarsi
+#include "automaton.hpp"
+#include <random>
+
+std::vector<Cell> Grid::state() { return grid_; }
+
+
+void Grid::evolve(int d){
+    
+}
