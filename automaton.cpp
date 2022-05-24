1198637d37300a9e691d3d593ba70dc9a679e4c3
Andrea Fraternali <andrea.fraternali4@studio.unibo.it>
2022-05-24 10:05:52 +0200

    bolo perchè continui a scrivere in italiano?

--- automaton.cpp
+++ automaton.cpp
@@ -1,4 +1,4 @@
-#include "automa.hpp"
+#include "automaton.hpp"
 
 //uso random per mettere un suscettibile in un punto a caso
 //uso random per confrontarlo con probabilità gamma di infettarsi
--- automaton.hpp
+++ automaton.hpp
@@ -1,26 +1,25 @@
 #ifndef AUTOMATON_HPP
-#define AUTOMATON_HPP
+#define AUTOMA_HPP
 
 
+#include <vector>
+#include<random>
 
+  enum class Cell {S, I, R};
 
+class Grid{
+    int side_;
+    std::vector<Cell> row;
+    std::vector<std::vector<Cell>> column; //perchè non posso fare vettore di righe?
+    
+    public: Grid(int side): side_(side)  {};
+     
+    for(int i=0, i<side, ++i) { //???
+    row.push_back(Cell::S);
+    }
+    
+     
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
 
 #endif
